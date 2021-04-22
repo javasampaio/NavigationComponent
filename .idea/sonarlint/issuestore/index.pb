4eaa906fbdf565b5d9724ed81bf90ca7af8d3e89
leandro.sampaio <leandro.sampaio@hbsis.com.br>
2021-04-22 16:54:53 -0300

    navigation component test

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+l
+<app/src/main/java/br/com/sam/navigationteste/MainActivity.kt,f\f\ffcbd73bc45b72c03633c6986b9d91afacafaacc
+N
+dynamicfeatureone/build.gradle,c\a\ca567d46a4df2b5a37673b456650a201243d1a1c
+Q
+!dynamicfeaturesecond/build.gradle,5\b\5b678053692fb64da451d42f77ed3d67cc465c59
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+Š
+Zdynamicfeaturesecond/src/main/java/br/com/sam/dynamicfeaturesecond/SecondModuleFragment.kt,a\a\aae9587316a6dd80262c02fead7f97e4749859b4
+ƒ
+Sdynamicfeatureone/src/main/java/br/com/sam/dynamicfeatureone/FirstModuleFragment.kt,7\1\7175f2478ed6c74cb6cac02fe855633217ec2dd5
---
+++ navigationTeste/dynamicfeaturesecond/src/main/java/br/com/sam/dynamicfeaturesecond/SecondModuleFragment.kt
@@ -0,0 +1,22 @@
+package br.com.sam.dynamicfeaturesecond
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.second_fragment.*
+
+class SecondModuleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.second_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_first.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondModuleFragment_to_FirstModuleFragment)
+        }
+    }
+}
